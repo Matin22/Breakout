475553822e2624afe8965e05691cbd915ec03e43
Matin22 <gholami.matin@gmail.com>
2025-07-24 10:57:46 +0200

    support for level import from file

--- src/game_level.cpp
+++ src/game_level.cpp
@@ -50,17 +50,36 @@ void GameLevel::init(std::vector<std::vector<unsigned int>> tileData, unsigned i
     unsigned int width = tileData[0].size();
     float unit_width = levelWidth / static_cast<float>(width);
     float unit_height = levelHeight / static_cast<float>(height);
+    Texture2D blockTexture;
 
-    for (unsigned int y = 0; y < height, y++)
+    for (unsigned int y = 0; y < height; y++)
     {
         for (unsigned int x = 0; x < width; x++)
         {
-            if (tileData[y][x] == 1){
+            if (tileData[y][x] == 1) // solid block
+            { 
                 glm::vec2 pos(unit_width * x, unit_height * y);
                 glm::vec2 size(unit_width, unit_height);
-                GameObject obj(pos, size, ResourceManager::GetTexture("black_solid"), glm::vec3(0.8f, 0.8f, 0.7f));
+                Texture2D solidTexture = ResourceManager::GetTexture("bedrock");
+                GameObject obj(pos, size, solidTexture, glm::vec3(0.8f, 0.8f, 0.7f));
                 obj.IsSolid = true;
                 this->Bricks.push_back(obj);
+            }
+
+            else if (tileData[y][x] > 1) // non solid
+            {
+                blockTexture = ResourceManager::GetTexture("stone");
+                glm::vec3 color = glm::vec3(1.0f);
+                if      (tileData[y][x] == 2) blockTexture = ResourceManager::GetTexture("cobble");
+                else if (tileData[y][x] == 3) blockTexture = ResourceManager::GetTexture("iron");
+                else if (tileData[y][x] == 4) blockTexture = ResourceManager::GetTexture("emerald");
+                else if (tileData[y][x] == 5) blockTexture = ResourceManager::GetTexture("diamond");
+
+                glm::vec2 pos(unit_width * x, unit_height * y);
+                glm::vec2 size(unit_width, unit_height);
+
+                
+                this->Bricks.push_back(GameObject(pos, size, blockTexture, color));
             }
         }
     }
